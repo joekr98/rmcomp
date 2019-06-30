dc5daf3e96f011ab52415a6f8248e2bb18c52e07
Esteban Davila <davilaesteban98@gmail.com>
2019-06-30 00:00:08 -0500

    descriptive

---
+++ build/precache-manifest.45d8b9c7325c1228dfb6fbaaa3ec4093.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bebbae1dbdf58b947b2dcdd9dabe092",
+    "url": "/rmcomp/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/rmcomp/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "3f2378475fad062d336f",
+    "url": "/rmcomp/static/css/main.243dac92.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/rmcomp/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "3f2378475fad062d336f",
+    "url": "/rmcomp/static/js/main.3860f6cd.chunk.js"
+  },
+  {
+    "revision": "467d6a8747c8cdc5cd05",
+    "url": "/rmcomp/static/js/runtime~main.2f9a6b02.js"
+  }
+]);
